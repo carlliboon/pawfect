56b1561c85e5e257426ba2a305edde5d69d133a9
michael <carlliboon12@gmail.com>
2025-07-27 19:34:47 +0800

    Refactor code structure for improved readability and maintainability

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+export const metadata = {
+  title: "Home",
+};
+
+export default function Home() {
+  return (
+    <Button variant="default" className="cursor-pointer">
+      Click Me
+    </Button>
+  );
+}
